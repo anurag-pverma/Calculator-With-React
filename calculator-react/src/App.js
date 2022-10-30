920b81a947586b9196b1f151d423de33e7e70679
Anurag Verma <anurag.premaverma@gmail.com>
2022-10-30 15:28:48 +0530

    add

---
+++ calculator-react/src/App.js
@@ -0,0 +1,70 @@
+
+import './App.css';
+
+
+import {useState, useRef, useEffect} from"react"
+
+
+function App() {
+  const [result, setResult] = useState("")
+  const inputRef = useRef(null);
+
+  useEffect(()=>{
+  inputRef.current.focus();
+  }, [])
+
+
+  const handleClick=(e)=>{
+    setResult(result.concat(e.target.name))
+  }
+
+  const handleClear =()=>{
+    setResult("")
+  }
+
+  const backspace=()=>{
+    setResult(result.slice(0, result.length-1))
+  }
+
+
+  const handleCalculate = ()=>{
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult("Error")
+    }
+  }
+  return (
+    <>
+    <div className='calc-app'>
+
+      <form>
+        <input type="text"  value={result}  ref={inputRef}/>
+      </form>
+
+      <div className="keypad">
+        <button id='clear'onClick={handleClear}>Clear</button>
+        <button id='backspace' onClick={backspace}>C</button>
+        <button name='+' onClick={handleClick} >+</button>
+        <button name='7' onClick={handleClick} >7</button>
+        <button name='8' onClick={handleClick} >8</button>
+        <button name='9' onClick={handleClick} >9</button>
+        <button name='-' onClick={handleClick} >-</button>
+        <button name='4' onClick={handleClick} >4</button>
+        <button name='5' onClick={handleClick} >5</button>
+        <button name='6' onClick={handleClick} >6</button>
+        <button name='*' onClick={handleClick} >&#215;</button>
+        <button name='1' onClick={handleClick} >1</button>
+        <button name='2' onClick={handleClick} >2</button>
+        <button name='3' onClick={handleClick} >3</button>
+        <button name='/' onClick={handleClick} >/</button>
+        <button name='.' onClick={handleClick} >.</button>
+        <button name='0' onClick={handleClick} >0</button>
+        <button name='='  id='result' onClick={handleCalculate}>=</button>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
