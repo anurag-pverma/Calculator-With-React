1a915d779231abbdf1a52b610362c07de94a29f5
Anurag Verma <anurag.premaverma@gmail.com>
2022-11-09 22:12:19 +0530

    ui changes

---
+++ calculator-react/src/Componetns/One.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+
+function One() {
+
+    const [first, setfirst] = useState(second)
+  return (
+    <div>
+
+
+
+    </div>
+  )
+}
+
+export default One
